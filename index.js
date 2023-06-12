1c3c55d68febd4f8b838808662934dac7a6668d0
steedude <85280987+steedude@users.noreply.github.com>
2023-06-12 18:07:25 +0800

    back to init

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Import packages
 const express = require("express");
-const cors = require('cors');
 const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 // Routes
 app.use("/home", home);
--- routes/home.js
+++ routes/home.js
@@ -3,38 +3,8 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    "items":[
-      {
-        "id": 1,
-        "image" : "discount.png",
-        "description" : "特價品"
-      },
-      {
-        "id": 2,
-        "image" : "handshake.png",
-        "description" : "握手"
-      },
-      {
-        "id": 3,
-        "image" : "not-found.png",
-        "description" : "似乎有錯誤"
-      },
-      {
-        "id": 4,
-        "image" : "molecule.png",
-        "description" : "化學"
-      },
-      {
-        "id": 5,
-        "image" : "paw.png",
-        "description" : "貓掌"
-      },
-      {
-        "id": 6,
-        "image" : "underground.png",
-        "description" : "地鐵"
-      }
-    ]
+    title: "Express Testing",
+    message: "The app is working properly!",
   });
 });
 
